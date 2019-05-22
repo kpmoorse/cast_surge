2738079d1a452316cd54b808cfdedfb7f71bf20a
Kellan Moorse <k.p.moorse@gmail.com>
2019-05-22 15:13:54 -0400

    Separating flystate and flyloc modules

--- PRISM/fly.pm
+++ PRISM/fly.pm
@@ -3,7 +3,7 @@ dtmc
 const double a = 0.1; // MC walk transition probability
 const double b = 0.9; // Correct Cast<->Surge transition probability
 const double c = 0.1; // Erroneous Cast<->Surge transition probability
-const double d = 0.1; // Arena return probability per step
+const double d = 0.9; // Arena return probability per step
 const int ext = 100; // Arena extent
 const int plume = 10; // Plume half-width
 const int ang = 0;
@@ -17,50 +17,47 @@ module flystate
 	// 0 - Cast init
 	// 1 - Cast left
 	// 2 - Cast right
-	// 3 - Cast out
+	// 3 - Cast out-of-bounds
 	// 4 - Surge
 	// 5 - Failure
 	// 6 - Success
 	s : [0..6] init 0;
+	
+	// Randomly initialize direction
+	[ini] (s=0) -> 0.5 : (s'=1) + 0.5 : (s'=2);
+
+	// If Cast, MC walk over directions
+	[step] (s=1) -> 1-a : (s'=1) + a : (s'=2);
+	[step] (s=2) -> a : (s'=1) + 1-a : (s'=2);
+
+	// Change state based on stochastic plume sensing
+	[step] (s=1 | s=2) & (x>-plume & x<plume) -> b : (s'=4) + 1-b : (s'=s);
+	[step] (s=1 | s=2) & !(x>-plume & x<plume) -> c : (s'=4) + 1-c : (s'=s);
+	[step] s=4 & !(x>-plume & x<plume) -> b: (s'=0) + 1-b : (s'=s);
+	[step] s=4 & (x>-plume & x<plume) -> c: (s'=0) + 1-c : (s'=s);
+
+	// If near target, success
+	[end] (s=1 | s=2 | s=4) & (pow(x,2)+pow(y,2)<pow(10,2)) -> 1.0 : (s'=6);
+
+	// If past target, fail
+	[end] s=4 & (y=0) -> 1.0 : (s'=5);
+
+endmodule
+
+module flyloc
 
 	// X & Y cartesian coordinates
 	x : [-ext..ext] init x0;
 	y : [0..ext] init ext;
 
-	// Update flag (true=state, false=coords)
-	z : bool init true;
-	
-	// Randomly initialize direction
-	[] (z=true & s=0) -> 0.5 : (s'=1) + 0.5 : (s'=2);
+	// Cast & surge
+	[step] (s=1 & x!=-ext) -> 1.0 : (x'=x-1);
+	[step] (s=2 & x!=ext)  -> 1.0 : (x'=x+1);
+	[step] (s=4 & y!=0) -> 1.0 : (y'=y-1);
 
-	// If Cast, MC walk over directions
-	[] s=1 -> 1-a : (s'=1) + a : (s'=2);
-	[] s=2 -> a : (s'=1) + 1-a : (s'=2);
-
-	// If outside arena, return after exp. dist. time
-	[] s=1 & x<=-ext -> 1.0 : (s'=3);
-	[] s=3 & x<=-ext -> d: (s'=2) & (x'=-ext+1) + 1-d : (s'=s);
-	[] s=2 & x>=ext -> 1.0 : (s'=3);
-	[] s=3 & x<=-ext -> d: (s'=1) & (x'=ext-1) + 1-d : (s'=s);
-
-	// Change state based on stochastic plume sensing
-	[] (s=1 | s=2) & (x>-plume & x<plume) -> b : (s'=4) + 1-b : (s'=s);
-	[] (s=1 | s=2) & !(x>-plume & x<plume) -> c : (s'=4) + 1-c : (s'=s);
-	[] s=4 & !(x>-plume & x<plume) -> b: (s'=0) + 1-b : (s'=s);
-	[] s=4 & (x>-plume & x<plume) -> c: (s'=0) + 1-c : (s'=s);
-
-	// If near target, success
-	[] (s=1 | s=2 | s=4) & (pow(x,2)+pow(y,2)<pow(10,2)) -> 1.0 : (s'=6);
-
-	// If past target, fail
-	//[] s=1 & (x=-ext) -> 1.0 : (s'=5);
-	//[] s=2 & (x=ext)  -> 1.0 : (s'=5);
-	[] s=4 & (y=0) -> 1.0 : (s'=5);
-
-	// Increment location
-	[] s=1 & (x!=-ext) -> 1.0 : (x'=x-1);
-	[] s=2 & (x!=ext)  -> 1.0 : (x'=x+1);
-	[] s=4 & (y!=0) -> 1.0 : (y'=y-1);
+	// Return from outside arena in exp. dist. time
+	[step] (x=-ext) -> d : (x'=x+1) + 1-d : (x'=x);
+	[step] (x=ext) -> d : (x'=x-1) + 1-d : (x'=x);
 
 endmodule
 
--- PRISM/test.pm
+++ PRISM/test.pm
@@ -3,29 +3,34 @@ dtmc
 const int ext = 100; // Arena size
 const double a = 0.2; // Levy flight transition probability
 
-module walker
+module walker_state
 
-	// Update flag (true=state, false=coords)
-	z : bool init true;
 	// local state
 	s : [0..2] init 0;
-	// Bounded location
-	x : [-ext..ext] init 0;
 	
 	// Initialize direction
-	[] (z=true & s=0) -> 0.5 : (s'=1) & (z'=false) + 0.5 : (s'=2) & (z'=false);
+	[ini] (s=0) -> 0.5 : (s'=1) + 0.5 : (s'=2);
 
 	// Update local state
-	[] (z=true & s!=0 & x<=-ext) -> 1.0 : (s'=2) & (z'=false);
-	[] (z=true & s!=0 & x>=ext) -> 1.0 : (s'=1) & (z'=false);
-	[] (z=true & s!=0 & x>=-ext+1 & x<=ext-1) -> a : (s'=3-s) & (z'=false) + 1-a : (s'=s) & (z'=false);
-
-	// Unbiased random walk
-	[] (z=false & s=1) -> (x'=x-1) & (z'=true);
-	[] (z=false & s=2) -> (x'=x+1) & (z'=true);
+	[step] (s!=0 & x<=-ext) -> 1.0 : (s'=2);
+	[step] (s!=0 & x>=ext) -> 1.0 : (s'=1);
+	[step] (s!=0 & x>=-ext+1 & x<=ext-1) -> a : (s'=3-s) + 1-a : (s'=s);
 	
 endmodule
 
+
+module walker_location
+
+	// Bounded location
+	x : [-ext..ext] init 0;
+
+	// Unbiased random walk
+	[step] (s=1 & x>=-ext+1) -> 1.0 : (x'=x-1);
+	[step] (s=2 & x<=ext-1) -> 1.0 : (x'=x+1);
+
+endmodule
+
+
 rewards "steps"
-	s!=0 & z=false : 1;
+	s!=0 : 1;
 endrewards
